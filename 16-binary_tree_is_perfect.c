a774151d8bbee9b901869c1cd504e82891349dfe
Soledad Frechou <solefrechou@hotmail.com>
2021-03-10 14:35:48 +0000

    Betty fix 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left) &&
+		    binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 
